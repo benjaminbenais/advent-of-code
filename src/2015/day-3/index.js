b24be408dd6a24b92641b26befbd6f5f7634012c
Benjamin Benais <benaisbenjamin@gmail.com>
2021-06-29 12:29:15 +0200

    refactor(2015/day-3): logic updated

--- src/2015/day-3/index.js
+++ src/2015/day-3/index.js
@@ -1,26 +1,55 @@
 const fs = require('fs');
 
 const puzzleInput = fs.readFileSync('./input.txt', 'utf-8');
-const directions = puzzleInput.split('');
+const mainDirections = puzzleInput.split('');
 
-let x = 0;
-let y = 0;
+const registerLocations = (directions) => {
+  const locations = new Set();
+  let x = 0;
+  let y = 0;
 
-const visitedLocations = new Set();
+  directions.forEach((direction) => {
+    locations.add(`${x},${y}`);
 
-directions.forEach((direction) => {
-  visitedLocations.add(`${x},${y}`);
+    if (direction === '^') {
+      y += 1;
+    } else if (direction === '>') {
+      x += 1;
+    } else if (direction === 'v') {
+      y -= 1;
+    } else {
+      x -= 1;
+    }
+  });
 
-  if (direction === '^') {
-    y += 1;
-  } else if (direction === '>') {
-    x += 1;
-  } else if (direction === 'v') {
-    y -= 1;
-  } else {
-    x -= 1;
-  }
-});
+  return locations;
+};
 
-const result = visitedLocations.size;
-console.log('result :', result);
+const result1 = registerLocations(mainDirections).size;
+console.log('result1 :', result1);
+
+const generateDirections = (directions) => {
+  const santaDirections = [];
+  const robotSantaDirections = [];
+
+  directions.forEach((direction, index) => {
+    if (index % 2 === 0) {
+      santaDirections.push(direction);
+    } else {
+      robotSantaDirections.push(direction);
+    }
+  });
+
+  return { santaDirections, robotSantaDirections };
+};
+
+const { santaDirections, robotSantaDirections } =
+  generateDirections(mainDirections);
+
+const santaLocations = registerLocations(santaDirections);
+const roboSantaLocations = registerLocations(robotSantaDirections);
+
+const mergedLocations = new Set([...santaLocations, ...roboSantaLocations]);
+
+const result2 = mergedLocations.size;
+console.log('result2 :', result2);
